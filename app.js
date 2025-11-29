8672b820b0edd621cdd3a17d24afce6ed8c80cfd
Mehdi Shakiba <Shakiba1983@gmail.com>
2025-11-30 00:23:54 +0330

    توضیح تغییرات جدید

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-const uuid = require ("uuid")
 
-console.log(uuid.v4)
+const express = require ("express")
+require ('dotenv').config()
+
+const app = express ()
+// app.get()
+// app.post()
+// app.put()
+// app.delete()
+app.get("/", (req , res)=>{
+    res.send("home page")
+})
+
+app.get("/app/courses", (req , res)=>{
+    res.send(['html', 'css' , 'java'])
+})
+
+app.get("/app/courses/:id", (req, res) => {
+    res.send([req.params.id])
+})
+
+app.get("/app/courses/:id/:name", (req, res) => {
+    res.send([req.params.id, req.params.name])
+})
+
+
+
+const port = process.env.APP_PORT || 3000
+app.listen(port, () => {
+    console.log(`listening port ${port}`)
+})
